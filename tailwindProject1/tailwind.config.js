7147eb60d29ccb4980cf954cf5dde7e8e73c4454
towhid <niamaaktar@gmail.com>
2022-07-15 21:48:11 +0600

    tailwindcssProject1 folde gets updated

--- tailwindProject1/tailwind.config.js
+++ tailwindProject1/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        "3xl": "0 40px 15px rgba(0, 0, 0, 0.4)",
+        "4xl": [
+          "0 35px 35px rgba(0, 0, 0, 0.25)",
+          "0 45px 65px rgba(0, 0, 0, 0.15)",
+        ],
+      },
+      gridTemplateColumns: {
+        "gird-cards": "repeat(auto-fit, minmax(15rem, 1fr))",
+      },
+    },
   },
   plugins: [],
 };
